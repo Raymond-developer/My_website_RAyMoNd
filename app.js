da51b56e23cb42c64af4ab1e76ec06c586138edd
Raymond <limax4644l@gmail.com>
2026-07-17 01:12:58 +0100

    first comit

---
+++ app.js
@@ -0,0 +1,156 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const user = require('./model/user')
+const mongodb = require('./model/schema')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const path = require('path');
+const dotenv = require('dotenv').config()
+
+const port = process.env.PORT || 8000
+const JWT_SECRET = process.env.JWT_SECRET
+const MONGO_URL = process.env.MONGO_URL
+
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'frontend')))
+
+  let token = '';
+
+ let User = '';
+
+  app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'register.html'));
+ })
+
+   app.get('/get', (req, res) => {
+    res.json({
+      id: 'kjhgfhjksjhgjskjdhv',
+      name: 'fav',
+      email: 'jdhfgvhkfjh'
+    }, {
+      id: 'asiudyfgvcbxncbvsakjdfh',
+      name: 'moyi',
+      email: 'khfghdsgfd'
+    })
+   })
+
+ app.post('/api/change', async (req, res) => {
+  //console.log(req.body)
+   const { token, newPassword } = req.body
+    
+    if(!newPassword) {
+       return res.json({status: 'error', error: 'Invalid password'})
+
+     }  else if(newPassword.length <= 5) {
+       return res.json({status: 'error', error: 'password should be at least 6 character'})
+    }  
+
+   try{
+   const Userr = jwt.verify(token, JWT_SECRET)
+    //console.log(Userr)
+ 
+    const password = await bcrypt.hash(newPassword, 10)
+   const email = Userr.email
+   await user.updateOne(
+    { email }, 
+          {
+             $set:  { password }
+          }
+   )
+
+    res.json({status: 'ok'})
+
+   }catch(err) {
+    console.log(err)
+    res.json({ status: 'error', error: 'failed' })
+   }
+ })
+  
+
+ app.post('/login', async (req, res) => {
+  console.log(req.body)
+  const {name, email, password} = req.body
+
+     User = await user.findOne({email}).lean()
+     console.log(User)
+   
+   if(!User) {
+    return res.json({status: 'error', error: 'Invalid username/password'})
+   }
+    
+
+    const bcryptcheck = await bcrypt.compare(password, User.password)
+
+   
+    if(!bcryptcheck) {  
+        return res.json({status: 'error', error: 'invalid password'})
+    } 
+
+    token = jwt.sign({ 
+        email: User.email, 
+        userid: User.name
+        }, JWT_SECRET )
+   
+
+   res.json({status:'ok', data: token })
+}) 
+
+ 
+app.post('/register', async (req, res) => {
+   //console.log(req.body)
+   const {name, email, passwords} = req.body
+
+    if(!name || typeof name !== 'string') {
+       return res.send({status: 'error', error: 'Invalid username'})
+
+    } else if(!email) {
+       return res.send({status: 'error', error: 'Invalid email'})
+    }
+
+     if(!passwords) {
+       return res.send({status: 'error', error: 'Invalid password'})
+     } 
+     if(passwords.length <= 4) {
+       return res.send('password should be at least 6 character')
+    }
+
+
+   const password = await bcrypt.hash(passwords, 10)
+
+   try {
+   const response = await user.create({
+      name,
+      email,
+      password
+    })
+
+    console.log('users created successfully', response)
+
+   } catch(err) {
+     if(err.code === 11000) {
+       return res.send({status: 'error', error: 'Username already in use'})
+     }
+     throw err  
+   }
+
+   res.json({status:'ok'})
+})
+
+
+app.post('/create', (req, res) => {
+   const {select, input} = req.body
+
+  res.json({status: 'ok', data: select, input: input})
+})
+
+app.listen(port, () => {
+  console.log(`app is running on port ${port} `)  
+}) 
---
+++ model/schema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const UserSchem = new mongoose.Schema({
+     inn: { type: String, required: true},
+     name: { type: String, required: true},
+     username: { type: String, required: true},
+     email: { type: String, required: true},
+     input: { type: String, required: true}
+  },
+    { collection: 'product' }
+  ) 
+  const mongodb = mongoose.model('schema', UserSchem)
+
+  module.exports = mongodb
